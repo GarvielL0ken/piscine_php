8c54c1b66b3d675e4106ebe5d7f857d5cf72df4a
GarvielL0ken <jedmikasarkis@gmail.com>
2019-10-19 18:56:54 +0200

    fixed it

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -4,27 +4,59 @@ window.onload = function()
 {
     var button = document.getElementById('new');
     button.addEventListener('click', promptToDo);
+    createList();
 }
+
+function createList()
+{
+    cookie = getCookie('list_items');
+    if (cookie != "")
+    {
+        arr_text = cookie.split(',');
+        for (var i = arr_text.length - 1; i >= 0; i--)
+            addListItem(arr_text[i]);
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) 
+    {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') 
+      {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0)
+      {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
 function promptToDo()
 {
     var userToDo = prompt("Enter a new item for the to do list", "skydiving");
     if (userToDo != null && userToDo != '')
-        addToDo(userToDo);
+        addListItem(userToDo);
 }
 
-function addToDo(userToDo)
+function addListItem(userToDo)
 {
     var list = document.getElementById('ft_list');
     var newItem = document.createElement('div');
     var newText = document.createTextNode(userToDo);
     newItem.appendChild(newText);
+    newItem.id = list_index;
     newItem.onclick = function() {
         removeListItem(this);
     };
     list.insertBefore(newItem, list.childNodes[0]);
     list_index++;
-    saveToCookie();
+    setCookie();
 }
 
 function removeListItem(item)
@@ -33,13 +65,14 @@ function removeListItem(item)
         item.remove();
 }
 
-function saveToCookie(){
-    var ft_list = document.getElementById('ft_list');
-    var todo = ft_list.children;
-    var newCookie = [];
-    for (var i = 0; i < todo.length; i++)
-        newCookie.unshift(todo[i].innerHTML);
-    document.cookie= "todos=" + JSON.stringify(newCookie);
-    cookie = document.cookie;
-    console.log(cookie);
+function setCookie(){
+    var cookie = "list_items= ";
+    var list_items = document.getElementById("ft_list");
+    list_items = list_items.children;
+    for (var i = list_items.length - 1; i >= 0; i--)
+    {
+        var div = document.getElementById(i);
+        cookie = cookie + ',' + div.innerText;
+    }
+    document.cookie = cookie;
 };
